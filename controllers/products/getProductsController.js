3620168e3fe462e133f6f42249167e2e1fb099b4
SooliduS <amirreza.setorg@gmail.com>
2023-05-10 15:11:59 +0430

    fix swagger get products

--- controllers/products/getProductsController.js
+++ controllers/products/getProductsController.js
@@ -1,53 +1,69 @@
-const Product = require('../../models/productModel')
+const Product = require('../../models/productModel');
 
-const getAllProducts = async (req ,res) => {
-    try{
-        const products = await Product.find()
-        return res.status(200).json(products)
-    }catch(e){
-        return res.status(500).json({message:e.message})
+const getAllProducts = async (req, res) => {
+    try {
+        const products = await Product.find();
+        return res.status(200).json(products);
+    } catch (e) {
+        return res.status(500).json({ message: e.message });
     }
-}
+};
 
-const getAmazingOfferProducts = async (req , res) => {
-    try{
-        const products = await Product.find({isAmazingOffer:true , discount:{$gt:5}})
+const getAmazingOfferProducts = async (req, res) => {
+    try {
+        const products = await Product.find({
+            isAmazingOffer: true,
+            discount: { $gt: 5 },
+        });
 
-        return res.status(200).json(products)
-    }catch(e){
-        return res.status(500).json({message:e.message})
+        return res.status(200).json(products);
+    } catch (e) {
+        return res.status(500).json({ message: e.message });
     }
-}
+};
 
-const getNewestProducts = async (req ,res) => {
-    try{
-        const products = await Product.find().sort({createdAt:-1}).limit(20)
+const getNewestProducts = async (req, res) => {
+    try {
+        const products = await Product.find().sort({ createdAt: -1 }).limit(20);
 
-        return res.status(200).json(products)
-
-    }catch(e){
-        return res.status(500).json({message:e.message})
+        return res.status(200).json(products);
+    } catch (e) {
+        return res.status(500).json({ message: e.message });
     }
-}
+};
 
-const getMostSalesProducts = async (req , res) => {
-    try{
-        const products = await Product.find({ordersCount:{$gt:1}}).sort({ordersCount:-1}).limit(20)
+const getMostSalesProducts = async (req, res) => {
+    try {
+        const products = await Product.find({ ordersCount: { $gt: 1 } })
+            .sort({ ordersCount: -1 })
+            .limit(20);
 
-        return res.status(200).json(products)
-    }catch(e){
-        return res.status(500).json({message:e.message})
+        return res.status(200).json(products);
+    } catch (e) {
+        return res.status(500).json({ message: e.message });
     }
-}
+};
 
-const getProduct = async (req , res) => {
-    const productId = req.params.productId || req.params.product_id || req.params.id
+const getProduct = async (req, res) => {
+    const productId =
+        req.params.productId || req.params.product_id || req.params.id;
 
-    if(!productId) return res.status(400).json({message:'product id needed'})
+    if (!productId)
+        return res.status(400).json({ message: 'product id needed' });
 
-    const product = await Product.findById(productId)
+    try {
+        const product = await Product.findById(productId);
 
-    return res.status(200).json(product)
-}
+        return res.status(200).json(product);
+    } catch (e) {
+        return res.sendStatus(500)
+    }
+};
 
-module.exports = { getAllProducts, getAmazingOfferProducts, getNewestProducts, getMostSalesProducts, getProduct }
+module.exports = {
+    getAllProducts,
+    getAmazingOfferProducts,
+    getNewestProducts,
+    getMostSalesProducts,
+    getProduct,
+};
--- routes/products/getProducts.js
+++ routes/products/getProducts.js
@@ -1,62 +1,138 @@
-const router = require('express').Router()
-const { getAllProducts , getAmazingOfferProducts , getMostSalesProducts , getNewestProducts , getProduct } = require('../../controllers/products/getProductsController')
+const router = require('express').Router();
+const {
+    getAllProducts,
+    getAmazingOfferProducts,
+    getMostSalesProducts,
+    getNewestProducts,
+    getProduct,
+} = require('../../controllers/products/getProductsController');
 
 /**
  * @swagger
  * /getproducts/all:
  *   get:
- *     summary: get all products product
- *     tags: [Get Products]
+ *     summary: Get all products
+ *     tags:
+ *       - GetProducts
+ *     description: Get all products as an array
+ *     produces:
+ *       - application/json
  *     responses:
- *       '200':
- *         description:'ok'
+ *       200:
+ *         description: All products
  *         content:
  *           application/json:
  *             schema:
- *               $ref: '#/components/schemas/Product'
- */         
-router.get('/all' , getAllProducts)
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Product'
+ *       500:
+ *         description: Internal server error
+ */
+router.get('/all', getAllProducts);
 
 /**
  * @swagger
- * /getproducts/amaizingoffers:
+ * /getproducts/amazingoffers:
  *   get:
- *     summary: get products in amazing offer
- *     tags: [GetProducts]
+ *     summary: Get amazing offers products
+ *     tags:
+ *       - GetProducts
+ *     description: Get amazing offers products as an array
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: amazing offers products
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Product'
+ *       500:
+ *         description: Internal server error
  */
-router.get('/amazingOffers' , getAmazingOfferProducts)
+router.get('/amazingoffers', getAmazingOfferProducts);
 
 /**
  * @swagger
- * /getproducts/mostsales:
+ * /getproducts/mostSales:
  *   get:
- *     summary: get most sales products
- *     tags: [GetProducts]
+ *     summary: Get mostSales products
+ *     tags:
+ *       - GetProducts
+ *     description: Get mostSales products as an array
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: most sales products
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Product'
+ *       500:
+ *         description: Internal server error
  */
-router.get('/mostSales', getMostSalesProducts)
+router.get('/mostSales', getMostSalesProducts);
 
 /**
  * @swagger
  * /getproducts/newest:
  *   get:
- *     summary: get newest products
- *     tags: [GetProducts]
+ *     summary: Get newest products
+ *     tags:
+ *       - GetProducts
+ *     description: Get newest products as an array
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: newest products
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Product'
+ *       500:
+ *         description: Internal server error
  */
-router.get('/newest' , getNewestProducts)
+router.get('/newest', getNewestProducts);
 
 /**
  * @swagger
- * /products/single/{id}:
+ * /getproducts/single/{id}:
  *   get:
  *     summary: Get a single product by ID
+ *     tags:
+ *       - GetProducts
+ *     description: Get a single product by its ID
  *     parameters:
- *       - in: path
- *         name: id
+ *       - name: id
+ *         in: path
  *         description: ID of the product to retrieve
  *         required: true
  *         schema:
  *           type: string
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: The product object
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Product'
+ *       404:
+ *         description: Product not found
+ *       500:
+ *         description: Internal server error
  */
-router.get('/single/:id' , getProduct)
+router.get('/single/:id', getProduct);
 
-module.exports = router
+
+module.exports = router;
