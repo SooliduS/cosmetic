88280ce58951902c514f351baf7ffd38ed6956a4
SooliduS <amirreza.setorg@gmail.com>
2023-05-22 20:12:25 +0430

    cors update

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,8 +2,9 @@ const allowedOrigins = [
     'https://www.yoursite.com',
     'http://localhost:3000',
     'http://localhost:3500',
-    'https://cosmetic-shop-tau.vercel.app/',
-    'https://cosmetic-backend.iran.liara.run/'
+    'https://cosmetic-shop-tau.vercel.app',
+    'https://cosmetic-backend.iran.liara.run',
+    'https://cosmetic-backend.onrender.com',
 ];
 
 module.exports = allowedOrigins;
